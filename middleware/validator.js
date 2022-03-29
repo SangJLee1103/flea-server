baad732e86c50176b4209d54305a115c963f66f0
sangjun <jesusfollow1103@naver.com>
2022-03-29 15:47:36 +0900

    FEAT: member join validation(회원가입 데이터 유효성 처리)

--- middleware/validator.js
+++ middleware/validator.js
@@ -3,7 +3,7 @@ const { validationResult } = require("express-validator");
 exports.validatorErrorChecker = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(409).json({ errors: errors.array() });
+    return res.status(409).json({ message: errors.array() });
   }
   next();
 }
--- routes/member.js
+++ routes/member.js
@@ -13,54 +13,60 @@ const router = express.Router();
 //회원가입
 router.route('/join')
     .post(
-        body("id").isLength({min: 8, max: 20}),
-        body("email").isEmail(),
-    
-        async (req, res, next) => {
-        const userData = req.body;
-        console.log(userData);
+        body("id").matches(/^[a-z]+[a-z0-9]{5,19}$/).withMessage("아이디는 최소 6자리 최대 20자리입니다."),
+        body("password").matches(/^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+]).{8,16}$/).withMessage("비밀번호는 8~ 20 자리이고 대소문자 또는 특수기호를 최소 1자 사용해야 합니다."),
+        body("email").isEmail().withMessage("이메일 형식이어야 합니다."),
+        body("phone").isMobilePhone().matches(/^01(?:0|1|[6-9])-(?:\d{3}|\d{4})-\d{4}$/).withMessage("휴대폰 번호 형식이어야합니다."),
+        body("nickname").isLength({min: 1, max: 30}).withMessage("닉네임은 15자리 이하입니다."),
+        body("address").isLength({min: 2, max: 50}).withMessage("주소를 간단하게라도 입력해주세요.(ex.서울특별시 강남구)"),
+        validatorErrorChecker,
         
+        async (req, res, next) => {
+            const userData = req.body;
+            console.log(userData);
+            
+            
+            //중복 체크
+            const userIdValidateion = await User.findOne({ where: { id: req.body.id }});
+            if (userIdValidateion) {
+                next('이미 등록된 아이디입니다.');
+                return;
+            }
 
-        const userIdValidateion = await User.findOne({ where: { id: req.body.id }});
-        if (userIdValidateion) {
-            next('이미 등록된 아이디입니다.');
-            return;
-        }
+            const userEmailValidation = await User.findOne({ where: { email: req.body.email }});
+            if (userEmailValidation) {
+                next('이미 등록된 메일입니다.');
+                return;
+            }
 
-        const userEmailValidation = await User.findOne({ where: { email: req.body.email }});
-        if (userEmailValidation) {
-            next('이미 등록된 메일입니다.');
-            return;
-        }
+            const userPhoneValidation = await User.findOne({ where: { phone: req.body.phone }});
+            if (userPhoneValidation) {
+                next('이미 등록된 휴대폰 번호입니다.');
+                return;
+            }
 
-        const userPhoneValidation = await User.findOne({ where: { phone: req.body.phone }});
-        if (userPhoneValidation) {
-            next('이미 등록된 휴대폰 번호입니다.');
-            return;
-        }
-
-        const userNicknameValidation = await User.findOne({ where: { nickname: req.body.nickname }});
-        if (userNicknameValidation) {
-            next('이미 등록된 닉네임입니다.');
-            return;
-        }
+            const userNicknameValidation = await User.findOne({ where: { nickname: req.body.nickname }});
+            if (userNicknameValidation) {
+                next('이미 등록된 닉네임입니다.');
+                return;
+            }
 
 
-        try {
-            const hash = await bcrypt.hash(req.body.password, 12);
-            await User.create({
-                id: req.body.id,
-                password: hash,
-                email: req.body.email,
-                phone: req.body.phone,
-                nickname: req.body.nickname,
-                address: req.body.address,
-            });
-            res.status(201).json({"message" : "회원가입이 완료되었습니다"});
-        } catch (err) {
-            console.log(err);
-            next(err);
-        }
+            try {
+                const hash = await bcrypt.hash(req.body.password, 12);
+                await User.create({
+                    id: req.body.id,
+                    password: hash,
+                    email: req.body.email,
+                    phone: req.body.phone,
+                    nickname: req.body.nickname,
+                    address: req.body.address,
+                });
+                res.status(201).json({"message" : "회원가입이 완료되었습니다"});
+            } catch (err) {
+                console.log(err);
+                next(err);
+            }
     });
 
 
