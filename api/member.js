a1db4151abe2a4a64465baf28a6d3b99ad2166b2
sangjun <jesusfollow1103@naver.com>
2022-04-08 19:02:37 +0900

    FEAT: Board (로그인한 회원이 올린 게시글 단건 조회, 해당 게시글 수정 API) FEAT: User(회원가입 및 회원수정 데이터 공백에 대한 validation 추가)

--- api/member.js
+++ api/member.js
@@ -15,11 +15,11 @@ const router = express.Router();
 //회원가입
 router.post('/join',
         // 회원가입 데이터 유효성 검증
-        body("id").matches(/^[a-z]+[a-z0-9]{5,19}$/).withMessage("아이디는 최소 6자리 최대 20자리입니다."),
-        body("password").matches(/^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+]).{8,16}$/).withMessage("비밀번호는 8~ 20 자리이고 대소문자 또는 특수기호를 최소 1자 이상 사용해야 합니다."),
-        body("email").isEmail().withMessage("이메일 형식이어야 합니다."),
-        body("phone").isMobilePhone().matches(/^01(?:0|1|[6-9])-(?:\d{3}|\d{4})-\d{4}$/).withMessage("휴대폰 번호 형식이어야합니다."),
-        body("nickname").isLength({min: 1, max: 30}).withMessage("닉네임은 15자리 이하입니다."),
+        body("id").trim().notEmpty().withMessage("공백불가!").matches(/^[a-z]+[a-z0-9]{5,19}$/).withMessage("아이디는 최소 6자리 최대 20자리입니다."),
+        body("password").trim().notEmpty().withMessage("공백불가!").matches(/^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+]).{8,16}$/).withMessage("비밀번호는 8~ 20 자리이고 대소문자 또는 특수기호를 최소 1자 이상 사용해야 합니다."),
+        body("email").trim().notEmpty().withMessage("공백불가!").isEmail().withMessage("이메일 형식이어야 합니다."),
+        body("phone").trim().notEmpty().withMessage("공백불가!").isMobilePhone().matches(/^01(?:0|1|[6-9])-(?:\d{3}|\d{4})-\d{4}$/).withMessage("휴대폰 번호 형식이어야합니다."),
+        body("nickname").trim().notEmpty().withMessage("공백불가!").isLength({min: 1, max: 30}).withMessage("닉네임은 15자리 이하입니다."),
         body("address").isLength({min: 2, max: 50}).withMessage("주소를 간단하게라도 입력해주세요.(ex.서울특별시 강남구)"),
         validatorErrorChecker, 
         async (req, res, next) => {
@@ -138,11 +138,11 @@ router.get('/info', auth, async(req, res, next) => {
 
 // 회원정보 수정
 router.put('/update', auth, 
-    body("id").matches(/^[a-z]+[a-z0-9]{5,19}$/).withMessage("아이디는 최소 6자리 최대 20자리입니다."),
-    body("password").matches(/^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+]).{8,16}$/).withMessage("비밀번호는 8~ 20 자리이고 대소문자 또는 특수기호를 최소 1자 이상 사용해야 합니다."),
-    body("email").isEmail().withMessage("이메일 형식이어야 합니다."),
-    body("phone").isMobilePhone().matches(/^01(?:0|1|[6-9])-(?:\d{3}|\d{4})-\d{4}$/).withMessage("휴대폰 번호 형식이어야합니다."),
-    body("nickname").isLength({min: 1, max: 30}).withMessage("닉네임은 15자리 이하입니다."),
+    body("id").trim().notEmpty().withMessage("공백불가!").matches(/^[a-z]+[a-z0-9]{5,19}$/).withMessage("아이디는 최소 6자리 최대 20자리입니다."),
+    body("password").trim().notEmpty().withMessage("공백불가!").matches(/^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+]).{8,16}$/).withMessage("비밀번호는 8~ 20 자리이고 대소문자 또는 특수기호를 최소 1자 이상 사용해야 합니다."),
+    body("email").trim().notEmpty().withMessage("공백불가!").isEmail().withMessage("이메일 형식이어야 합니다."),
+    body("phone").trim().notEmpty().withMessage("공백불가!").isMobilePhone().matches(/^01(?:0|1|[6-9])-(?:\d{3}|\d{4})-\d{4}$/).withMessage("휴대폰 번호 형식이어야합니다."),
+    body("nickname").trim().notEmpty().withMessage("공백불가!").isLength({min: 1, max: 30}).withMessage("닉네임은 15자리 이하입니다."),
     body("address").isLength({min: 2, max: 50}).withMessage("주소를 간단하게라도 입력해주세요.(ex.서울특별시 강남구)"),
     validatorErrorChecker, async(req, res, next) => {
         const userEmailDuplication = await User.findAll({ where: { email: req.body.email }});
