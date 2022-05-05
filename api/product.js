3e3b07562534abe64ee7eb6de7cf710c6b68f5ca
sangjun <jesusfollow1103@naver.com>
2022-05-05 17:05:00 +0900

    FEAT: 상품 수정, 삭제, 조회, 게시글 내부 상품 조회 API

--- api/product.js
+++ api/product.js
@@ -27,49 +27,49 @@ const upload = multer({
             cb(null, path.basename(file.originalname, ext) + Date.now() + ext);
         },
     }),
-    limits: { fileSize: 10 * 1024 * 1024 }, 
+    limits: { fileSize: 10 * 1024 * 1024 },
 });
 
 
 //상품 등록 API
 router.post('/:id/register', auth, upload.array('img', 5),
-        async (req, res, next) => {
-            try {
-                const user = await User.findOne({ where : { id: req.user } }); //로그인 한 회원 찾기
-                const board = await Board.findOne({ where : { id: req.params.id } });
-                const image = req.files;
-                const path = image.map(img => img.path);
-                
-                await Product.create({
-                    name: req.body.name,
-                    cost_price: req.body.cost_price,
-                    selling_price: req.body.selling_price,
-                    like_count: 0,
-                    description: req.body.description,
-                    board_id: board.id,
-                    user_id: user.id,
-                    img: path.toString() //이미지 경로 배열을 문자열로 변환
-                });
-                res.status(201).json({ message: "상품 등록 완료되었습니다.📚" });
-            } catch (err) {
-                console.log(err);
-                next(err);
-            }
-        }
-    )
+    async (req, res, next) => {
+        try {
+            const user = await User.findOne({ where: { id: req.user } }); //로그인 한 회원 찾기
+            const board = await Board.findOne({ where: { id: req.params.id } });
+            const image = req.files;
+            const path = image.map(img => img.path);
 
-//상품 수정, 삭제 API
-router.route('/:user_id/:board_id/:id')
+            await Product.create({
+                name: req.body.name,
+                cost_price: req.body.cost_price,
+                selling_price: req.body.selling_price,
+                like_count: 0,
+                description: req.body.description,
+                board_id: board.id,
+                user_id: user.id,
+                img: path.toString() //이미지 경로 배열을 문자열로 변환
+            });
+            res.status(201).json({ message: "상품 등록 완료되었습니다.📚" });
+        } catch (err) {
+            console.log(err);
+            next(err);
+        }
+    }
+)
+
+//상품 수정, 삭제, 조회 API
+router.route('/:board_id/:user_id/:id')
     .put(auth, upload.array('img', 5),
         async (req, res, next) => {
             try {
-                const user = await User.findOne({ where : { id: req.user } }); //로그인 한 회원 찾기
-                const board = await Board.findOne({ where : { id: req.params.id } });
-                const product = await Product.findOne({ where : { id: req.params.id} });
+                const board = await Board.findOne({ where: { id: req.params.board_id } });
+                const user = await User.findOne({ where: { id: req.user } }); //로그인 한 회원 찾기
+                const product = await Product.findOne({ where: { id: req.params.id } });
 
                 const image = req.files;
                 const path = image.map(img => img.path);
-                const toModifiedProduct = await Product.findOne({ where : { user_id: user.id, board_id: board.id, id: product.id }});
+                const toModifiedProduct = await Product.findOne({ where: { board_id: board.id, user_id: user.id, id: product.id } });
 
                 await toModifiedProduct.update({
                     name: req.body.name,
@@ -88,6 +88,52 @@ router.route('/:user_id/:board_id/:id')
             }
         }
     )
+    .delete(auth,
+        async (req, res, next) => {
+            try {
+                const board = await Board.findOne({ where: { id: req.params.board_id } });
+                const user = await User.findOne({ where: { id: req.user } }); //로그인 한 회원 찾기
+                const product = await Product.findOne({ where: { id: req.params.id } });
+                const toDeletedProduct = await Product.findOne({ where: { board_id: board.id, user_id: user.id, id: product.id } });
+
+                await toDeletedProduct.destroy();
+                res.status(200).json({ message: "상품이 삭제되었습니다.🚮" });
+            } catch (err) {
+                console.log(user);
+                console.log(err);
+                next(err);
+            }
+        }
+    )
+    .get(auth,
+        async (req, res, next) => {
+            try {
+                const board = await Board.findOne({ where: { id: req.params.board_id } });
+                const user = await User.findOne({ where: { id: req.user } }); //로그인 한 회원 찾기
+                const product = await Product.findOne({ where: { id: req.params.id } });
+                const toFindProduct = await Product.findOne({ where: { board_id: board.id, user_id: user.id, id: product.id } });
+                
+                res.status(200).json({ message: toFindProduct });
+            } catch (err) {
+                console.log(user);
+                console.log(err);
+                next(err);
+            }
+        }
+    )
+
+//하나의 게시글에 있는 모든 상품 조회 API
+router.get('/:board_id', auth, async(req, res, next) => {
+    try{
+        const board = await Board.findOne({ where: { id: req.params.board_id } });
+        const productAll = await Product.findAll({ where: {board_id: board.id} });
+
+        res.status(200).json({message: productAll});
+    } catch (err) {
+        console.log(err);
+        next(err);
+    }
+}) 
 
 
 
